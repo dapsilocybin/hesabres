823acdfd5950a7cb12f9494e54f1a1ba68c2ac47
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2025-11-22 09:49:43 +0330

    Update theme_cubit.dart

--- lib/core/theme/theme_cubit.dart
+++ lib/core/theme/theme_cubit.dart
@@ -1,1 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'app_theme.dart';
+import 'color_presets.dart';
+import 'theme_state.dart';
+
+class ThemeCubit extends Cubit<ThemeState> {
+  ThemeCubit()
+      : super(
+          ThemeState(
+            mode: ThemeMode.system,
+            seedColor: AppColorPresets.blue,
+          ),
+        );
+
+  void setThemeMode(ThemeMode mode) => emit(state.copyWith(mode: mode));
+
+  void setSeedColor(Color color) => emit(state.copyWith(seedColor: color));
+
+  ThemeData get light => AppTheme.light(state.seedColor);
+  ThemeData get dark => AppTheme.dark(state.seedColor);
+}
 
